28295af0a27e31abaeacfe3cf088c6dea5573934
TheChiefCode <dvdbreen3@gmail.com>
2023-04-20 16:39:51 -0400

    added controller functionality

--- src/pyqt_render.py
+++ src/pyqt_render.py
@@ -13,7 +13,7 @@ class Space(object):
     def __init__(self, parent=None):
 
         self.w = gl.GLViewWidget(parent=parent)
-        self.w.setGeometry(0,110,1920,1080)
+        self.w.setGeometry(0, 110, 1920, 1080)
         self.w.setWindowTitle('Orbit')
         self.w.setCameraPosition(distance=30, elevation=8)
 
@@ -26,9 +26,7 @@ class Space(object):
         self.time = 1
         self.dt = 0.01
         self.refresh_rate = int((1000/60))
-        self.run_time = 0.01
-        
-
+        self.run_time = 0.1
 
     def addGrid(self):
         
@@ -36,32 +34,26 @@ class Space(object):
         grid.scale(2, 2, 2)
         self.w.addItem(grid)
 
-    
     def set_dt(self, dt):
 
         self.dt = dt
-
 
     def setCameraPos(self):
 
         self.w.cameraPosition()
 
-
     def addBodies(self, models):
 
         self.bodies.append(models)
 
-    
     def addSatellites(self, models):
 
         self.satellites.append(models)
-
 
     def addInputs(self, widget):
 
         self.inputs.append(widget)
 
-    
     def update(self):
         
         for i in self.inputs:
@@ -85,7 +77,6 @@ class Space(object):
             self.time = self.time+self.dt
         else:
             self.time = 0
-
 
     def animation(self):
         
@@ -152,7 +143,8 @@ class Sphere(object):
 
 class Cubesat(object):
 
-    def __init__(self, name='', controller=None, orbit=None, camera=True, magnetorquers=True):
+    def __init__(self, name='', controller=None, orbit=None, camera=True,
+                 magnetorquers=True):
 
         self.name = name
         self.controller = controller
@@ -172,44 +164,48 @@ class Cubesat(object):
                                [0, 2, 4], [1, 4, 5], [1, 5, 7],
                                [5, 6, 4], [5, 6, 7], [3, 6, 7],
                                [3, 2, 6], [6, 2, 4], [0, 2, 6]])
-        
+
         self.modeld = gl.MeshData(vertexes=self.verticies, faces=self.faces)
         self.modelw = gl.GLMeshItem(meshdata=self.modeld, drawEdges=True)
-        self.modelc = gl.MeshData.cylinder(rows=10, cols=20, radius=[0.1, 2.0], 
+        self.modelc = gl.MeshData.cylinder(rows=10, cols=20, radius=[0.1, 3.2],
                                            length=5.)
 
         self.colors = np.ones((self.modelc.faceCount(), 4), dtype=float)
-        self.colors[::2,0] = 0
-        self.colors[:,1] = np.linspace(0, 1, self.colors.shape[0])
+        self.colors[::2, 0] = 0
+        self.colors[:, 1] = np.linspace(0, 1, self.colors.shape[0])
         self.modelc.setFaceColors(self.colors)
 
         self.modelcw = gl.GLMeshItem(meshdata=self.modelc, smooth=True,
                                      drawEdges=True, edgeColor=(1, 0, 0, 1),
                                      shader='balloon')
 
-        self.modell = gl.GLLinePlotItem(pos=[0,0,0])
+        self.modell = gl.GLLinePlotItem(pos=[0, 0, 0])
         self.modell.setParentItem(self.modelw)
-        
-        self.angular_velocity = np.array([0, 0, 0], ndmin=2)
+
+        self.angular_velocity = np.array([0.3, 0.25, 0.1], ndmin=2)
         self.rot = np.array([1, 0, 0, 0], float, ndmin=2)
         self.pos = np.array([0, 0, 0], ndmin=2)
         self.moments = np.array([0, 0, 0])
-        self.setPoint = np.array([0, 1, 1])  # defined in ECEF
+        self.setPointECEF = np.array([0, 1, 0])  # defined in ECEF
+        self.setPointBF = np.array([0, 1, 0])
 
-        self.kp = 0.0001
-        self.kd = 10
-        self.error = np.array([0, 0, 0])
-        self.derivative = np.array([0, 0, 0])
-        self.lastError = np.array([0, 0, 0])
+        self.kp = 0.1
+        self.kd = 1
+        self.km = -10
+        self.error = 0
+        self.derivative = 0
+        self.lastError = 0
+        self.BFieldECEF = np.array([0, 0, 1])
         self.BField = np.array([0, 0, 1])
-        self.antennaVector = np.array([0, 0, -1])
+        self.BDot = np.array([0, 0, 0])
+        self.antennaVector = np.array([0, 0, 1])
 
     def updateCube(self):
 
         self.modelw.resetTransform()
         self.modelw.translate(self.pos[-1][0],self.pos[-1][1],self.pos[-1][2])
         new_rot = quaternion_2_euler_aa(self.rot[-1])
-        self.modelw.rotate(new_rot[0], new_rot[1], new_rot[2], new_rot[3], 
+        self.modelw.rotate(new_rot[0], new_rot[1], new_rot[2], new_rot[3],
                            local=True)
         if (self.camera):
             self.modelcw.resetTransform()
@@ -219,13 +215,10 @@ class Cubesat(object):
         if (self.magnetorquers):
             lastRot = quaternion_hamilton(self.rot[-2], self.rot[-1])
 
-            self.BField = quaternion_rotation_matrix((lastRot),
-                                                     self.BField)
-            self.setPoint = quaternion_rotation_matrix((lastRot),
-                                                     self.setPoint)
-            self.modell.setData(pos=[[0,0,0],self.setPoint])
-            print(self.setPoint)
-
+            self.BField = quaternion_rotation_matrix((self.rot[-1]),
+                                                     self.BFieldECEF)
+            self.setPointBF = quaternion_rotation_matrix((self.rot[-1]),
+                                                          self.setPointECEF)
 
     def scaleObject(self):
 
@@ -234,28 +227,41 @@ class Cubesat(object):
 
     def moveObject(self, new_pos):
 
-        self.pos = np.append(self.pos, [new_pos], axis = 0)
+        self.pos = np.append(self.pos, [new_pos], axis=0)
 
     def newRotation(self, new_rot):
 
-        self.rot = np.append(self.rot, [new_rot], axis = 0)
+        self.rot = np.append(self.rot, [new_rot], axis=0)
 
     def actuatorMoment(self, dutyCycle):
         # dutyCycle is a 1x3 array of the duty cycle of the acturators
         # add check for duty cycle shape in future
         saturationField = np.array([0.1, 0.1, 0.1])  # In Tesla I think
         # Unit vectors for the magnetorquer forces maths
-        quatVector = np.cross(self.setPoint, self.BField)
-        if(np.linalg.norm(quatVector)!=0):
-            quatVector = np.divide(quatVector, np.linalg.norm(quatVector))
-        dipoleVector = np.cross(quatVector, self.BField)
-        if(np.linalg.norm(dipoleVector)!=0):
+        # used to be setPointBF and BField. Changing to antenna Vector and 
+        # setpointBF
+        '''quatVector = np.add(np.cross(self.antennaVector, self.setPointBF)*self.kp,
+                            np.cross(self.angular_velocity[-1], self.BField)*100)
+        print(self.angular_velocity[-1])
+        if(np.linalg.norm(quatVector) != 0):
+            quatVector = np.divide(quatVector, np.linalg.norm(quatVector))'''
+        dipoleVector = np.add(np.cross(self.antennaVector, self.setPointBF)* dutyCycle,
+                              np.cross(self.angular_velocity[-1], self.BField)*self.km)
+        if(np.linalg.norm(dipoleVector) != 0):
             dipoleVector = np.divide(dipoleVector, np.linalg.norm(dipoleVector))
+        # signVector = np.sign(self.setPointBF-self.antennaVector)
+        # dipoleVector = np.multiply(dipoleVector, signVector)
         # Multiply the unit vectors by the field strength to get the
         # saturation force
         saturationForce = np.multiply(saturationField, dipoleVector)
+        magneticTorque = np.cross(self.BField, saturationForce)
         # This can then be controlled by the controller by duty cycling
-        self.moments = np.multiply(saturationForce, dutyCycle)
+        self.moments = magneticTorque
+        self.modell.setData(pos=[[0, 0, 0], self.setPointBF*10,
+                                 [0, 0, 0], self.BField*5,
+                                 [0, 0, 0], dipoleVector*5,
+                                 [0, 0, 0], self.antennaVector*5])
+        #print(dutyCycle)
 
     # THIS NEEDS WORK!!! ITS HARD CODED FOR ONLY ONE INPUT
     # THIS NEEDS TO BE IN A QUATERNION FORMAT
@@ -263,24 +269,26 @@ class Cubesat(object):
 
         Setpoint = [self.controller.x,
                     self.controller.y,
-                    self.controller.z]
+                    1]
         Setpoint = np.divide(Setpoint, np.linalg.norm(Setpoint))
-
-        self.setPoint = Setpoint
-
-        # print(self.setPoint)
+        # remember this***
+        self.BFieldECEF = Setpoint
 
     def controllerPD(self):
 
         self.lastError = self.error
 
-        self.error = np.arccos(np.divide(np.dot(self.setPoint, self.antennaVector), np.multiply(np.linalg.norm(self.setPoint), np.linalg.norm(self.antennaVector))))
-        derivative = (self.error + self.lastError)
-        
+        dotP = np.dot(self.setPointBF, self.antennaVector)
+        normA = np.linalg.norm(self.antennaVector)
+        normB = np.linalg.norm(self.setPointBF)
+        divNorm = dotP / (normA * normB)
+        self.error = np.arccos(divNorm)
+        # This has a singularity at 0, but should be fine
+        #print(self.error)
 
-        adjust = (self.kp*self.error + self.kd*derivative)
-
-        adjust = np.clip(adjust, -1, 1)
+        self.derivative = (self.error - self.lastError)
+        adjust = (self.kp*self.error + self.kd*self.derivative)
+        #adjust = np.clip(adjust, -1, 1)
 
         return adjust
 
@@ -288,7 +296,7 @@ class Cubesat(object):
 
         # THIS IS A TEMPORARY BODGE
         # FIND A CLEANER MORE FLEXIBLE WAY
-        #self.controllerInput()
+        self.controllerInput()
         self.actuatorMoment(self.controllerPD())
 
         new_angular_velocity, new_rot = simulate_dynamics(self.Itensor,
@@ -298,7 +306,7 @@ class Cubesat(object):
                                                        run_time,
                                                        dt,
                                                        just_last=True)
-              
+
         self.rot = np.append(self.rot, [new_rot], axis=0)
         self.angular_velocity = np.append(self.angular_velocity,
                                           [new_angular_velocity], axis=0)
@@ -313,7 +321,6 @@ class Satjoystick(object):
         self.x = 0
         self.y = 0
         self.z = 0
-        
 
     def update(self):
 
@@ -352,7 +359,6 @@ class Actuator(object):
         pass
 
 
-
 if __name__ == '__main__':
 
     app = QtWidgets.QApplication(sys.argv)
@@ -362,20 +368,15 @@ if __name__ == '__main__':
     win.setCentralWidget(cw)
     layout = QtWidgets.QGridLayout()
     cw.setLayout(layout)
-    
-
-    
 
     sim_space = Space()
-    layout.addWidget(sim_space.w,0,0,4,4)
+    layout.addWidget(sim_space.w, 0, 0, 4, 4)
     controls = Satjoystick()
     layout.addWidget(controls.w)
 
     moon = Sphere()
     earth = Sphere()
     cubesat = Cubesat(controller=controls)
-    
-  
 
     win.show()
 
@@ -385,10 +386,10 @@ if __name__ == '__main__':
     sim_space.addInputs(controls)
     sim_space.w.addItem(cubesat.modell)
 
-    moon.scaleObject([2,2,2])
-    moon.moveObject([20,-23,5])
-    earth.scaleObject([20,20,20])
-    earth.moveObject([0,25,0])
+    moon.scaleObject([2, 2, 2])
+    moon.moveObject([20, -23, 5])
+    earth.scaleObject([20, 20, 20])
+    earth.moveObject([0, 25, 0])
 
     sim_space.addGrid()
     sim_space.animation()
